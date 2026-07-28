13a44df0c119fc1d6edf17e2c7deeb7d3723b419
lau-ro19 <lroseantoinette@gmail.com>
2026-07-28 17:38:58 +0200

    feat: add _printf function implementation

--- _printf.c
+++ _printf.c
@@ -34,3 +34,60 @@ int write_text_block(const char *format, int *index)
 
 	return (total);
 }
+	/**
+ * _printf - produces output according to a format
+ * @format: character string containing format directives
+ *
+ * Return: number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	int count = 0, i = 0, printed;
+	va_list args;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(args, format);
+
+	while (format[i] != '\0')
+	{
+		printed = write_text_block(format, &i);
+		if (printed == -1)
+		{
+			va_end(args);
+			return (-1);
+		}
+		count += printed;
+
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == '\0')
+			{
+				va_end(args);
+				return (-1);
+			}
+
+			if (format[i] == 'c')
+				count += print_char(args);
+			else if (format[i] == 's')
+				count += print_string(args);
+			else if (format[i] == '%')
+				count += print_percent(args);
+			else if (format[i] == 'd' || format[i] == 'i')
+				count += print_digit(args);
+			else
+			{
+				_putchar('%');
+				_putchar(format[i]);
+				count += 2;
+			}
+			i++;
+		}
+	}
+
+	va_end(args);
+	return (count);
+
+}
