b009341bb2dbb101197cadd6ba60b95f407cf94d
lau-ro19 <lroseantoinette@gmail.com>
2026-07-28 17:00:29 +0200

    FIX: some include

---
+++ main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <stddef.h> 
+
+
+int _printf(const char *format, ...);
+int write_text_block(const char *format, int *index);
+int print_char(va_list args);
+int print_string(va_list args);
+int print_percent(va_list args);
+int print_digit(va_list args);
+
+#endif /* MAIN_H */
